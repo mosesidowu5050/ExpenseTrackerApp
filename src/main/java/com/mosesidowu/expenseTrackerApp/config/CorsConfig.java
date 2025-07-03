205140f54a379f0eb0d4773068d8703532f6d1fe
idowu moses <dynamopraize5050@gmail.com>
2025-07-03 07:30:43 +0100

    update project

--- src/main/java/com/mosesidowu/expenseTrackerApp/config/CorsConfig.java
+++ src/main/java/com/mosesidowu/expenseTrackerApp/config/CorsConfig.java
@@ -13,7 +13,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://127.0.0.1:5500")
+                        .allowedOrigins("http://127.0.0.1:5501")
                         .allowedMethods("GET", "POST", "PUT", "DELETE")
                         .allowedHeaders("*")
                         .allowCredentials(true);
--- src/main/java/com/mosesidowu/expenseTrackerApp/controller/ExpenseController.java
+++ src/main/java/com/mosesidowu/expenseTrackerApp/controller/ExpenseController.java
@@ -17,7 +17,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/expenses")
-@CrossOrigin(origins = "http://127.0.0.1:5500")
+@CrossOrigin(origins = "http://127.0.0.1:5501")
 public class ExpenseController {
 
     @Autowired
